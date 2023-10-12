c5eaf08416c16c6cb29258cf18fd63ddc27c3c5f
SadakovDmitry <143087297+SadakovDmitry@users.noreply.github.com>
2023-10-12 13:11:13 +0300

    Add files via upload

--- Read_file.h
+++ Read_file.h
@@ -30,6 +30,7 @@ enum Comands
 };
 
 struct SPU{
+    int* bin_buf;
     Elem_t* args;
     unsigned int SPU_err;
 };
@@ -37,11 +38,11 @@ struct SPU{
 
 int Size_of_text(int* Num_rows, FILE* file);
 struct About_str* Input_text (int Num_rows, char* buffer, FILE* file);
-void Make_file(struct About_text* ab_text, struct About_str* ab_str);
+void Make_bin_file(struct About_text* ab_text, struct About_str* ab_str);
 struct About_str* Work_with_input_file(struct About_text* ab_text, char* buffer, int* Num_rows, FILE* file);
 void Clean_buf();
 void fill_struct( struct About_text* ab_text, int* Num_rows, int size_of_text, char* buffer);
 enum Comands Convert_to_numbers(struct About_str* ab_str, char * now_comand, int i);
-void Convert_to_comands(char* str, FILE* output_file);
-void Convert_to_cheak_file(struct About_text* ab_text, struct About_str* ab_str);
-char* Work_with_bin_file(struct About_text* ab_text, char* buffer, int* Num_rows, FILE* file);
+int Convert_to_comands(struct SPU* spu, FILE* output_file);
+void Convert_to_cheak_file(struct About_text* ab_text, struct SPU* spu);
+int* Work_with_bin_file(struct About_text* ab_text, FILE* file);
