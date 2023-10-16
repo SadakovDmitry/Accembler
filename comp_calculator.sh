b800584d4ee9b6f0292964c07f5cf6155c5076d4
SadakovDmitry <143087297+SadakovDmitry@users.noreply.github.com>
2023-10-16 20:13:33 +0300

    Add files via upload

--- comp_calculator.sh
+++ comp_calculator.sh
@@ -1,13 +1,19 @@
 g++ -c Read_file.cpp -o Read_file.o
 g++ -c Assembler.cpp -o assembler.o
-g++ assembler.o Read_file.o -fsanitize=address -o assembler
+g++ -c SPU_func.cpp -o SPU_func.o
+g++ -c stack_functions.cpp -o stack_functions.o
+g++ assembler.o Read_file.o SPU_func.o stack_functions.o -fsanitize=address -o assembler
 ./assembler
 
 g++ -c stack_functions.cpp -o stack_functions.o
 g++ -c SPU_processor.cpp -o SPU_processor.o
-g++ Read_file.o SPU_processor.o stack_functions.o -o SPU
+g++ -c SPU_func.cpp -o SPU_func.o
+g++ Read_file.o SPU_processor.o stack_functions.o SPU_func.o -o SPU
 ./SPU
-
 
 g++ Convert_to_comands.cpp Read_file.cpp -fsanitize=address -o decoder
 ./decoder
+
+
+
+
