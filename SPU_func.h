3f8bf6bf5fb427a8db7bf0ff2ee85d513da26cb3
SadakovDmitry <143087297+SadakovDmitry@users.noreply.github.com>
2023-10-25 21:28:45 +0300

    Add files via upload

--- SPU_func.h
+++ SPU_func.h
@@ -1,7 +1,9 @@
 #define SPU_DUMP(spu) SPU_Dump(spu, (char*)__FILE__, __LINE__, __func__);
 #define NUM_ARGS 4
 
-void Do_comands (struct About_text* ab_text, struct Stack* stk, struct Canary* canary, struct SPU* spu);
+const int SIZE_OF_RAM = 20000;
+
+void Do_comands (struct About_text* ab_text, struct Stack* stk, struct Stack* stk_calls, struct Canary* canary, struct SPU* spu);
 
 unsigned int SPU_Verify(struct SPU* spu);
 void SPU_Dump(struct SPU* spu, char* file , int line, const char* func);
--- SPU_processor.cpp
+++ SPU_processor.cpp
@@ -20,25 +20,30 @@ int main()
     int capacity = 1;
     ab_text.first_or_second_file = 2;
 
+    spu.RAM = (Elem_t*) calloc (SIZE_OF_RAM, sizeof(Elem_t));
+
 
 
     FILE* file = fopen("code_bin.bin", "rb");
 
     spu.bin_buf = Work_with_bin_file(&ab_text, file);
 
-    /*
-    for (int i = 0; i < ab_text.text_size; i++)
-    {
-        printf("%d ", *(spu.bin_buf + i));
-    }
-    */
+    //Print_bin_buf(spu.bin_buf, ab_text.text_size);
 
     struct Stack* stk = Stack_Ctor( capacity, &ERR, &canary);
-    SPU_Ctor(stk, &canary, &spu);
+    struct Stack* stk_calls = Stack_Calls_Ctor( capacity, &ERR, &canary);
 
-    Do_comands (&ab_text, stk, &canary, &spu);
+    SPU_Ctor (stk, &canary, &spu);
 
-    SPU_Dtor(stk, &canary, &spu);
-    Stack_Dtor(stk, &canary);
+    Do_comands (&ab_text, stk, stk_calls, &canary, &spu);
+
+    SPU_Dtor (stk, &canary, &spu);
+
+    Stack_Dtor (stk, &canary);
+    Stack_Dtor (stk_calls, &canary);
+
+    Print_RAM (&spu, SIZE_OF_RAM);
+
+    Print_VRAM(&spu, SIZE_OF_RAM);
 
 }
